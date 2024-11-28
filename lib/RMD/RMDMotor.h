a2a2b3a8e15264844d68b59a61c8a4f5ed9286dc
drawal <2581478521@qq.com>
2024-11-28 15:50:36 +0800

    针对RMD，减除冗余的错误提示，统一错误提示输出风格，完善函数注释

--- lib/RMD/RMDMotor.h
+++ lib/RMD/RMDMotor.h
@@ -3,13 +3,14 @@
  * @author worranhin (worranhin@foxmail.com)
  * @author drawal (2581478521@qq.com)
  * @brief RMD motor class
- * @version 0.1
- * @date 2024-11-05
+ * @version 0.2
+ * @date 2024-11-28
  *
  * @copyright Copyright (c) 2024
  *
  */
 #pragma once
+#include "RobotException.hpp"
 #include <Windows.h>
 #include <cstdint>
 #include <iostream>
@@ -18,51 +19,45 @@
 namespace D5R {
 
 struct PIPARAM {
-  uint8_t angleKp;
-  uint8_t angleKi;
-  uint8_t speedKp;
-  uint8_t speedKi;
-  uint8_t torqueKp;
-  uint8_t torqueKi;
+    uint8_t angleKp;
+    uint8_t angleKi;
+    uint8_t speedKp;
+    uint8_t speedKi;
+    uint8_t torqueKp;
+    uint8_t torqueKi;
 };
 
 enum ID_ENTRY {
-  ID_01 = (uint8_t)0x01,
-  ID_02 = (uint8_t)0x02,
+    ID_01 = (uint8_t)0x01,
+    ID_02 = (uint8_t)0x02,
 };
 class RMDMotor {
-public:
-  RMDMotor();
-  RMDMotor(const char *serialPort, uint8_t id);
-  RMDMotor(HANDLE comHandle, uint8_t id);
-  ~RMDMotor();
-  bool Init();
-  bool isInit();
-  bool Reconnect();
-  bool GetMultiAngle_s(int64_t *angle);
-  uint16_t GetSingleAngle_s();
-  uint8_t GetHeaderCheckSum(uint8_t *command);
-  bool GoAngleAbsolute(int64_t angle);
-  bool GoAngleRelative(int64_t angle);
-  int16_t OpenLoopControl(int16_t power);
-  void SpeedControl(int32_t speed);
-  bool Stop();
-  bool SetZero();
-  bool GetPI();
-  bool WriteAnglePI(const uint8_t *arrPI);
-  bool DebugAnglePI(const uint8_t *arrPI);
+  public:
+    RMDMotor();
+    RMDMotor(HANDLE comHandle, uint8_t id);
+    ~RMDMotor();
+    int64_t GetMultiAngle_s();
+    uint16_t GetSingleAngle_s();
+    uint8_t GetHeaderCheckSum(uint8_t *command);
+    void GoAngleAbsolute(int64_t angle);
+    void GoAngleRelative(int64_t angle);
+    int16_t OpenLoopControl(int16_t power);
+    void SpeedControl(int32_t speed);
+    void Stop();
+    void SetZero();
+    void GetPI();
+    void WriteAnglePI(const uint8_t *arrPI);
+    void DebugAnglePI(const uint8_t *arrPI);
 
-  PIPARAM _piParam;
+    PIPARAM _piParam;
 
-  HANDLE _handle;
-  uint8_t _id;
+    HANDLE _handle;
+    uint8_t _id;
 
-private:
-  const char *_serialPort;
-  DWORD _bytesRead;
-  DWORD _bytesWritten;
-  bool _isInit;
-  uint8_t _checksum(uint8_t nums[], int start, int end);
+  private:
+    DWORD _bytesRead;
+    DWORD _bytesWritten;
+    uint8_t _checksum(uint8_t nums[], int start, int end);
 };
 
 } // namespace D5R
--- lib/RMD/SerialPort.h
+++ lib/RMD/SerialPort.h
@@ -1,15 +1,29 @@
+/**
+ * @file SerialPort.h
+ * @author worranhin (worranhin@foxmail.com)
+ * @author drawal (2581478521@qq.com)
+ * @brief SerialPort Class
+ * @version 0.2
+ * @date 2024-11-28
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #pragma once
+#include "RobotException.hpp"
+#include <iostream>
+#include <stdexcept>
 #include <windows.h>
 
 namespace D5R {
 class SerialPort {
-public:
-  SerialPort(const char *serialPort);
-  ~SerialPort();
-  HANDLE GetHandle();
+  public:
+    SerialPort(const char *serialPort);
+    ~SerialPort();
+    HANDLE GetHandle();
 
-private:
-  HANDLE _handle;
+  private:
+    HANDLE _handle;
 };
 
 } // namespace D5R
