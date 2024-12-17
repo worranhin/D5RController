5799b5f674b845773e06fe2bfddf9b035bf03de3
worranhin <worranhin@foxmail.com>
2024-12-17 15:12:57 +0800

    修复拼写错误，添加测试 DLL 程序

---
+++ test/TestDll.cpp
@@ -0,0 +1,13 @@
+﻿#include "DllApi.h"
+
+const std::string port = "\\\\.\\COM16";
+
+int main() {
+    using namespace D5R;
+
+    D5Robot* robot;
+    CreateD5RobotInstance(robot, port.c_str());
+    DestroyD5RobotInstance(robot);    
+    
+    return 0;
+}
